2c786cad3cc53db39b8ed46c050ef2afa42e2ff7
HeyThereGithub <70286042+HeyThereGithub@users.noreply.github.com>
2021-04-06 19:29:34 -0700

    Lab 16

---
+++ lab16/js/lab16.js
@@ -0,0 +1,15 @@
+//Lab16
+//
+//Author: Jeremy Jacob Francisco
+//Date: 4/6/2021
+//License: N/A
+//
+//
+//JavaScript for Web
+//Using JQuery
+
+$("#output").append("<p><button id='my-button'>Press This, Please!</button></p>")
+$("#my-output").click(function(){
+  alert("Thanks so much for doing what I asked.");
+})
+$("button").css("background-color", "red")
